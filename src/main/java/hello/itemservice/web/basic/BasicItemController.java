5cc142142ce721f84887da21aaab922463843ec6
devcsb <devcsb119@gmail.com>
2023-01-28 18:04:43 +0900

    상품 목록 - 타임리프
    스프링 MVC - 웹 페이지 만들기

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,43 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+//@RequiredArgsConstructor // final, @NonNull이 붙은 필드에 대한 생성자를 만들어줌.
+@Controller
+@RequestMapping("/basic/items")
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+/*    @Autowired  // 생성자가 하나만 있는 경우, @Autowired 생략 가능. 스프링이 자동으로 생성자에 @Autowired 를 붙여 의존성 주입을 해준다. */
+    public BasicItemController(ItemRepository itemRepository) {   // Lombok 의 @RequiredArgsConstructor 사용으로 대체 가능
+        this.itemRepository = itemRepository;
+    }
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
